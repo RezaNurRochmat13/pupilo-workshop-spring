e5324f451da047910bd9df04247fc1bb46f0d5a8
Reja <rezanurrochmat@gmail.com>
2022-12-04 11:11:27 +0700

    add postgre and h2 database

--- src/main/java/com/pupilo/MainApplication/controller/PersonController.java
+++ src/main/java/com/pupilo/MainApplication/controller/PersonController.java
@@ -1,10 +1,9 @@
 package com.pupilo.MainApplication.controller;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,41 +15,57 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pupilo.MainApplication.entity.Person;
+import com.pupilo.MainApplication.exception.ResourceNotFoundException;
+import com.pupilo.MainApplication.repository.PersonRepository;
+
 @RestController
 @RequestMapping("/api/")
 public class PersonController {
+    @Autowired
+    private PersonRepository personRepository;
     
     @GetMapping("persons")
     public ResponseEntity<Object> getAllPersons() {
-        List<String> persons = new ArrayList<>();
-        persons.add("Fikri");
-        persons.add("Amien");
-        persons.add("Indra");
-
-        return new ResponseEntity<>(persons, HttpStatus.OK);
-    }
-
-    @GetMapping("persons/{id}")
-    public ResponseEntity<Object> getPersonById(@PathVariable Integer id) {
-        return new ResponseEntity<>(id, HttpStatus.OK);
-    }
-
-    @PostMapping("persons")
-    public ResponseEntity<Object> createPerson(@RequestBody String payload) {
-        return new ResponseEntity<>(payload, HttpStatus.CREATED);
-    }
-
-    @PutMapping("persons/{id}")
-    public ResponseEntity<Object> updatePerson(@RequestBody String payload, @PathVariable Integer id) {
+        List<Person> persons = personRepository.findAll();
         Map<String, Object> map = new HashMap<>();
-        map.put("payload", payload);
-        map.put("id", id);
+        map.put("data", persons);
 
         return new ResponseEntity<>(map, HttpStatus.OK);
     }
 
+    @GetMapping("persons/{id}")
+    public ResponseEntity<Object> getPersonById(@PathVariable Long id) {
+        Person personById = personRepository.findById(id)
+                            .orElseThrow(() -> new ResourceNotFoundException("Person not found with id : " + id));
+        
+        return new ResponseEntity<>(personById, HttpStatus.OK);
+    }
+
+    @PostMapping("persons")
+    public ResponseEntity<Object> createPerson(@RequestBody Person payload) {
+        return new ResponseEntity<>(personRepository.save(payload), HttpStatus.CREATED);
+    }
+
+    @PutMapping("persons/{id}")
+    public ResponseEntity<Object> updatePerson(@RequestBody Person payload, @PathVariable Long id) {
+        Person personById = personRepository.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException("Person not found with id : " + id));
+
+        personById.setName(payload.getName());
+        personById.setAge(payload.getAge());
+        personById.setAddress(payload.getAddress());
+
+        return new ResponseEntity<>(personRepository.save(personById), HttpStatus.OK);
+    }
+
     @DeleteMapping("persons/{id}")
-    public ResponseEntity<Object> deletePerson(@PathVariable Integer id) {
-        return new ResponseEntity<>(id, HttpStatus.OK);
+    public ResponseEntity<Object> deletePerson(@PathVariable Long id) {
+        Person personById = personRepository.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException("Person not found with id : " + id));
+
+        personRepository.delete(personById);
+
+        return new ResponseEntity<>("Successfullly delete", HttpStatus.NO_CONTENT);
     }
 }
---
+++ src/main/java/com/pupilo/MainApplication/repository/PersonRepository.java
@@ -0,0 +1,10 @@
+package com.pupilo.MainApplication.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pupilo.MainApplication.entity.Person;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
